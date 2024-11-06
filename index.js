22f3d47afd7c6fdde735bbe4cde68018272b9df0
Jamey Jones <jpkey764@gmail.com>
2024-11-06 16:02:25 -0500

    Entering first javascript

---
+++ index.js
@@ -0,0 +1,16 @@
+ //Message user that they will be opening an important vault with a secret combination.//
+ 
+ alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+
+ //Send math calculations that equal combination values to user//
+
+ let comB1 = 6 + 4;
+ let comB2 = 80/2;
+ let comB3 = 13 * 3;
+
+console.log (comB1);
+console.log (comB2);
+console.log (comB3);
+
+//Create dialog box that shows vault codes to user//
+alert("The vault codes are 10-40-39");
